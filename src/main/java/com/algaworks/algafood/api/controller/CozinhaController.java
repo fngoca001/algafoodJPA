c551e462bcac51eecc0f37d80d258940dbf85321
fngoca001 <137906919+fngoca001@users.noreply.github.com>
2023-07-10 22:49:38 +0200

    Atualizar cozinha

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -1,14 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
-
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -23,27 +24,42 @@ public class CozinhaController {
 
 	@Autowired
 	private CozinhasRepository cozinhasRepository;
-	
+
 	@GetMapping
-	public List<Cozinha> listar(){
-		return cozinhasRepository.listar();	
-		}
-	
+	public List<Cozinha> listar() {
+		return cozinhasRepository.listar();
+	}
+
 	@GetMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {	
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long cozinhaId) {
 		Cozinha cozinha = cozinhasRepository.porId(cozinhaId);
-		
+
 		if (cozinha != null) {
-			
+
 			return ResponseEntity.ok(cozinha);
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public Cozinha adicionar(@RequestBody Cozinha cozinha) {
 		return cozinhasRepository.adicionar(cozinha);
+	}
+
+	@PutMapping("/{cozinhaId}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable Long cozinhaId, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhasRepository.porId(cozinhaId);
+
+		if (cozinhaAtual != null) {
+			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
+
+			cozinhasRepository.adicionar(cozinhaAtual);
+
+			return ResponseEntity.ok(cozinhaAtual);
+		}
+		
+		return ResponseEntity.notFound().build();
 	}
 }
