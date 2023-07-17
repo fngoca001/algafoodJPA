758a628f9411a1862c1269b9a173c83f5a80b6bd
fngoca001 <137906919+fngoca001@users.noreply.github.com>
2023-07-17 11:08:23 +0200

    Aprendendo sobre pesquisas

--- src/main/java/com/algaworks/algafood/domain/repository/RestaurantesRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestaurantesRepository.java
@@ -1,9 +1,23 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algafood.domain.entity.Restaurante;
 
 public interface RestaurantesRepository extends JpaRepository<Restaurante, Long>{
 
+	//Retorna uma lista de restaurante num intervalo de taxa frete
+	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
+	
+	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinha); 
+	
+	Optional<Restaurante> findFirstRestauranteByNomeContaining(String nome);
+	
+	List<Restaurante>findTop2ByNomeContaining(String nome);
+	
+	int countByCozinhaId(Long cozinha);
 }
