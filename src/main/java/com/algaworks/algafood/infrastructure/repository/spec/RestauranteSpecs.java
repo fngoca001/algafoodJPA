c954f8eb529e40ac3b9d5f4fb39ca1139aa259cb
fngoca001 <137906919+fngoca001@users.noreply.github.com>
2023-07-20 06:45:55 +0200

    finalizando o quinto modolo spring data jpa

---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/spec/RestauranteSpecs.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.infrastructure.repository.spec;
+
+import java.math.BigDecimal;
+import org.springframework.data.jpa.domain.Specification;
+
+import com.algaworks.algafood.domain.entity.Restaurante;
+
+public class RestauranteSpecs {
+
+	public static Specification<Restaurante> comFreteGratis() {
+		return (root, query, builder) -> builder.equal(root.get("taxaFrete"), BigDecimal.ZERO);
+	}
+
+	public static Specification<Restaurante> comNomeCemeLhante(String nome) {
+		return (root, query, builder) -> builder.like(root.get("nome"), "%" + nome + "%");
+	}
+}
